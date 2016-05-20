9ffe8ff72554d60b15a3ca7382fc91eb6584cc1f
Jonatan Poza <jona-darknes@hotmail.com>
2016-05-20 14:42:36 -0400

    CRUD J.Poza / Cliente Mensajes Alerta en español

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,6 +39,7 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Fichas Mascotas', 'url' => ['/fichamascota/index']],
+            ['label' => 'Clientes', 'url' => ['/cliente/index']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
